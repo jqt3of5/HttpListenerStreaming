8770cd831076b84608f71ba7d2e2eb656eced975
jqt3of5 <jqt3of5@gmail.com>
2020-11-09 20:46:28 -0700

    Consumer/Producer for http streaming

--- HTTPStreaming/Program.cs
+++ HTTPStreaming/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Text.Unicode;
 using System.Threading;
@@ -11,70 +13,55 @@ namespace HTTPStreaming
 {
     class Program
     {
-        private static List<string> _data = new List<string>();
-        static void ProducerTask()
+        static async void ConsumerTask()
         {
-            int i = 0;
+            var handler = new HttpClientHandler();
+            var client = new HttpClient(handler);
+
             while (true)
             {
-                lock (_data)
-                {
-                    _data.Add("Data produced. Index: "+ i);
-                }
-                i++;
-                Thread.Sleep(1000);
+                Console.WriteLine("New connection");
+                var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Get, "http://localhost:8080/session/12345"),
+                    HttpCompletionOption.ResponseHeadersRead); 
+                    var stream = await response.Content.ReadAsStreamAsync();
+                    var readStream = new StreamReader(stream);
+                    while (!readStream.EndOfStream)
+                    {
+                        var buffer = new char[100];
+                        var length = await readStream.ReadAsync(buffer);
+                        Console.WriteLine("length: " + length);
+                        Console.WriteLine(buffer);     
+                    }
+                   
             }
         }
         static void Main(string[] args)
         {
-            //Start the producer
-            Task.Run(ProducerTask);
-            
             var listener = new HttpListener();
-            listener.Prefixes.Add("http://+:8080/session/12345/");
+            listener.Prefixes.Add("http://localhost:8080/session/12345/");
             listener.Start();
 
+            //Start the producer
+            Task.Run(ConsumerTask);
 
             while (true)
             {
                 var context = listener.GetContext();
-                // var request = context.Request;
-
+                
                 Task.Run(() =>
                 {
-                    int publishedIndex = 0; 
+                    int index = 0; 
+                    context.Response.SendChunked = true;
+                    context.Response.KeepAlive = true;
+                    context.Response.ContentType = "Application/octet-stream";
+                    
                     while (true)
                     {
-
-                        int count = publishedIndex;
-                        lock (_data)
-                        {
-                            count = _data.Count;
-                        }
-
-                        if (count <= publishedIndex)
-                        {
-                            Thread.Sleep(1000);
-                            continue;
-                        }
-                        
-                        List<string> response;
-                        lock (_data)
-                        {
-                            response = _data.Skip(publishedIndex).ToList();
-                            publishedIndex = _data.Count;
-                        }
-
-                        context.Response.SendChunked = true;
-                        var offset = 0;
-                        foreach (var str in response)
-                        {
-                            var buffer = Encoding.UTF8.GetBytes(str);
-                            context.Response.ContentLength64 = buffer.Length;
-                            context.Response.OutputStream.Write(buffer, offset, buffer.Length);
-                            // context.Response.OutputStream.Flush();
-                            offset += buffer.Length;
-                        }
+                        var buffer = Encoding.UTF8.GetBytes($"Content with data with index: {index}");
+                        context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                        context.Response.OutputStream.Flush();
+                        index += 1;
+                        Thread.Sleep(1000); 
                     }
                 });
             }
